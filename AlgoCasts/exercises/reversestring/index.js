0b2beb352a55ad87817f6ed39ab09f8fcb81d8f2
japerry911 <jack.e.perry@outlook.com>
2020-06-16 12:02:05 -0600

    ReverseString

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -2,10 +2,31 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution 3:
+function reverse(str) {
+    return str.split('').reduce((accumulator, currentCharacter) => currentCharacter + accumulator, '');
+}
 
 module.exports = reverse;
+
+// Solution 1:
+/*
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+*/
+
+// Solution 2:
+/*
+function reverse(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+*/
