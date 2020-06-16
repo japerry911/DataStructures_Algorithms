027fd4c83c3eb45799b04d409304e97eb0d5c362
japerry911 <jack.e.perry@outlook.com>
2020-06-16 17:19:07 -0600

    Capitalize

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -7,6 +7,35 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words = [];
+
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+}
 
 module.exports = capitalize;
+
+// Solution 1:
+/*
+let returnSentence = '';
+    let capitalizeBool = true;
+
+    for (let char of str) {
+        if (capitalizeBool) {
+            capitalizeBool = false;
+            returnSentence += char.toUpperCase();
+        } else {
+            if (char === ' ') {
+                capitalizeBool = true;
+            }
+
+            returnSentence += char;
+        }
+    }
+
+    return returnSentence;
+*/
