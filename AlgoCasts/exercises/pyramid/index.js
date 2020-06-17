cc01af2aa77f023caf2e8496ea766a4de6a15cdf
japerry911 <jack.e.perry@outlook.com>
2020-06-17 09:03:59 -0600

    Pyramid

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -13,7 +13,58 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+// Solution 2:
+function pyramid(n, row=0, pyramidRow='') {
+    if (n === row) {
+        return;
+    }
+
+    if (((n - 1) * 2) + 1 === pyramidRow.length) {
+        console.log(pyramidRow);
+        return pyramid(n, row + 1);
+    }
+
+    const min = n - row - 1;
+    const max = n + row - 1;
+
+    if (pyramidRow.length >= min && pyramidRow.length <= max) {
+        pyramidRow += '#';
+    } else {
+        pyramidRow += ' ';
+    }
+
+    pyramid(n, row, pyramidRow);
+}
+
+pyramid(2);
 
 module.exports = pyramid;
+
+// Solution 1: 
+/*
+function pyramid(n) {
+    const rowLength = ((n - 1) * 2) + 1;
+
+    for (let i = 0; i < n; i++) {
+        let pyramidRow = '';
+        for (let r = 0; r < rowLength; r++) {
+            const min = n - i;
+            const max = n + i;
+
+            if (r + 1 >= min && r + 1 <= max) {
+                pyramidRow += '#';
+            } else {
+                pyramidRow += ' ';
+            }
+        }
+
+        console.log(pyramidRow);
+    }
+}
+*/
